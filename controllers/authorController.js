6f1acdad12d596cfe262a5f5e976229d1eb34468
Lata Phartyal <lata12phartyal@gmail.com>
2023-08-10 21:53:38 +0530

    adding new file

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -65,5 +65,40 @@ const createAuthor = async function (req, res) {
   };
   
 
-
-module.exports.createAuthor = createAuthor;
+  const login = async function (req, res) {
+    try {
+      const data = req.body;
+      if (!data.email) {
+        return res
+          .status(400)
+          .send({ status: false, msg: "email is required field" });
+      }
+      if (!data.password) {
+        return res
+          .status(400)
+          .send({ status: false, msg: "password is required field" });
+      }
+      const authorMatch = await authorModel.findOne({
+        email: data.email,
+        password: data.password,
+      });
+      if (!authorMatch) {
+        
+        return res
+          .status(400)
+          .send({ status: false, msg: "email or password is incorrect" });
+      }
+      const token = jwt.sign({ authorId: authorMatch._id }, "lata12", {
+        expiresIn: "70h",
+      });
+      return res
+        .status(200)
+        .send({ status: true, msg: "Logged in successfully", token });
+    } catch (error) {
+      res.status(500).send({ status: false, error: error.msg });
+    }
+  };
+  
+  module.exports.login = login;
+  module.exports.createAuthor = createAuthor;
+  
